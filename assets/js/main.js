021df3d98e0c6afa015fbd3a83160af5868a3f72
Yevhen Trystan <kingguru@ukr.net>
2023-05-04 14:45:35 +0300

    optimize js code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,21 @@
-const tabItem = document.querySelectorAll('.tabs__btn-item');
-const tabContent = document.querySelectorAll('.tabs__content-item');
+let tabItem = document.querySelectorAll('.tabs__btn-item');
+let tabContent = document.querySelectorAll('.tabs__content-item');
 
 tabItem.forEach(function (el) {
-    el.addEventListener('click', open)
-})
+    el.addEventListener('click', open);
+});
 
-function open(event) {
-    const isTarget = event.currentTarget;
-    const btn = isTarget.dataset.button;
-
+function open() {
     tabItem.forEach(function (item) {
         item.classList.remove('tabs__btn-item--active');
     });
 
-    isTarget.classList.add('tabs__btn-item--active');
+    this.classList.add('tabs__btn-item--active');
 
     tabContent.forEach(function (item) {
-        item.classList.remove('tabs__content-item--active')
+        item.classList.remove('tabs__content-item--active');
     });
-    document.querySelector(`#${btn}`).classList.add('tabs__content-item--active');
+    document.querySelector('#' + this.dataset.button).classList.add('tabs__content-item--active');
 }
 
 const menuBtn = document.querySelector('.menu__btn');
